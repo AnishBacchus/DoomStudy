ac4380ac9c9fc3c3fddd6b0fb7c4ee7db9865dc2
Kush-Ramdut <knr042703@gmail.com>
2025-06-25 02:23:37 -0400

    Added light gray background color to website

--- my-frontend/src/App.js
+++ my-frontend/src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 
 function App() {
+  const palette = {
+    light_grey: "#8E9AAF",
+  };
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
+    <div
+      style={{
+        backgroundColor: palette.light_grey,
+        minHeight: "100vh",
+        padding: "50px",
+        textAlign: "center",
+        margin: 0,
+      }}
+    >
       <h1> DoomStudy</h1>
       <p>For all the academic weapons who suffer from brainrot</p>
     </div>
