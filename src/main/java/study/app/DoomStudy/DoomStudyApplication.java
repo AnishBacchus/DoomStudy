67d97de1de301829a443feee44912b9bd84bc83f
AnishBacchus <anishbacchus@gmail.com>
2025-06-24 23:56:03 -0400

    First Commit

---
+++ src/main/java/study/app/DoomStudy/DoomStudyApplication.java
@@ -0,0 +1,13 @@
+package study.app.DoomStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoomStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoomStudyApplication.class, args);
+	}
+		//Test
+}
