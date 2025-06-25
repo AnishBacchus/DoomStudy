8726324eb7ebb3ed6a5d64c9c71549376050d717
AnishBacchus <anishbacchus@gmail.com>
2025-06-25 00:14:50 -0400

    Setup for REST Controller

---
+++ src/main/java/Controller/StudyController.java
@@ -0,0 +1,43 @@
+package Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class StudyController {
+
+    //-----------------------------------------------------------------------------------------------------------
+    // Method to add prompt.
+
+    @PostMapping("/add")
+    public void addPrompt() { //Status 201
+
+    }
+    //-----------------------------------------------------------------------------------------------------------
+    //Method to get all the prompts created.
+
+    @GetMapping("/get")
+    public void getDrivers() { //Status 200
+
+    }
+
+    //-----------------------------------------------------------------------------------------------------------
+    //Method to replace an old prompt with new prompt.
+
+    @PutMapping("/replace/{current}")
+    public void updatePrompt(@PathVariable String current) { //Status 200
+
+    }
+
+    //-----------------------------------------------------------------------------------------------------------
+    //Method to delete a prompt.
+
+    @DeleteMapping("/remove/{prompt}")
+    public void removePrompt(@PathVariable String prompt) { //Status 204
+
+    }
+
+}
+
--- src/main/java/study/app/DoomStudy/DoomStudyApplication.java
+++ src/main/java/study/app/DoomStudy/DoomStudyApplication.java
@@ -9,5 +9,5 @@ public class DoomStudyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DoomStudyApplication.class, args);
 	}
-		//Testing
+
 }
